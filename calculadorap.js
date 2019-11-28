ba811200a69db05c1915cf1ef795bbfcd97c581d
Fabián <cbeltrani@ifp.mx>
2019-11-28 10:29:48 -0600

    as

---
+++ calculadorap.js
@@ -0,0 +1,19 @@
+alert('holaa')
+document.getElementById('boto2').addEventListener('click', oper)
+function oper() {
+  let opera = document.getElementById('ope').value
+  let nume1 = parseFloat(document.getElementById('num1').value)
+  let nume2 = parseFloat(document.getElementById('num2').value)
+  if (opera==='+' || opera==='suma'){
+    alert('El resultado es '+ (nume1+nume2))
+  }  
+if (opera ==='-' || opera==='resta'){
+  alert('El resultado es '+ (nume1-nume2))
+}
+if (opera ==='x' || opera==='multiplicación'){
+  alert('El resultado es '+ (nume1*nume2))
+}
+if (opera ==='/' || opera==='división'){
+  alert('El resultado es '+ (nume1/nume2))
+}
+}
